19bf288e5c91eac934a789755f3c411af8ad98dd
onkar <onkar@saeloun.com>
2022-05-04 20:43:35 +0530

    added img

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,12 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <h1> Hello World</h1>
-     {/* <Image
+     <Image
           // className={`object-cover border border-solid ${isFounder() ? 'cursor-pointer' : 'cursor-default'} grayscale`}
-
 src={'https://image.shutterstock.com/image-photo/batsman-hitting-cricket-ball-tournament-260nw-11474998.jpg'}          // alt={name}
           width={200}
           height={200}
-        /> */}
+        />
     </div>
   )
 }
