8050b27cb46c43a43eb467f54fcb841b69ca2bf8
onkar <onkar@saeloun.com>
2022-05-05 11:54:49 +0530

    added loader

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,27 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import React, { useEffect } from 'react'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const [loading, setLoading] = React.useState<boolean>(true);
+
+  useEffect(() => {
+    // setTimeout(() => {
+      setLoading(false);
+    // }, 1000);
+  }, []);
+  
   return (
+    <>
+    {loading ? <h2>Loading</h2> : (
     <div className={styles.container}>
       <h1> Hello World</h1>
       image with priority
      <Image
           // className={`object-cover border border-solid ${isFounder() ? 'cursor-pointer' : 'cursor-default'} grayscale`}
-src={'https://media.istockphoto.com/photos/lens-image-dslr-manhattan-downtown-city-new-york-hand-picture-id901169654?b=1&k=20&m=901169654&s=170667a&w=0&h=0SkF_wRuG7RqWhvmV0QM0xpIbZDYXokJMS_oEQs0S9U='}          // alt={name}
+          src={'https://media.istockphoto.com/photos/lens-image-dslr-manhattan-downtown-city-new-york-hand-picture-id901169654?b=1&k=20&m=901169654&s=170667a&w=0&h=0SkF_wRuG7RqWhvmV0QM0xpIbZDYXokJMS_oEQs0S9U='}          // alt={name}
           width={200}
           height={200}
           priority
@@ -18,11 +29,13 @@ src={'https://media.istockphoto.com/photos/lens-image-dslr-manhattan-downtown-ci
         Image without priority
         <Image
           // className={`object-cover border border-solid ${isFounder() ? 'cursor-pointer' : 'cursor-default'} grayscale`}
-src={'https://media.istockphoto.com/photos/lens-image-dslr-manhattan-downtown-city-new-york-hand-picture-id901169654?b=1&k=20&m=901169654&s=170667a&w=0&h=0SkF_wRuG7RqWhvmV0QM0xpIbZDYXokJMS_oEQs0S9U='}          // alt={name}
+          src={'https://media.istockphoto.com/photos/lens-image-dslr-manhattan-downtown-city-new-york-hand-picture-id901169654?b=1&k=20&m=901169654&s=170667a&w=0&h=0SkF_wRuG7RqWhvmV0QM0xpIbZDYXokJMS_oEQs0S9U='}          // alt={name}
           width={200}
           height={200}
         />
     </div>
+    )}
+    </>
   )
 }
 
